fe763ffdf78d4ae67841ffcd9ef4d0276150b58c
Ben Ramsey <ben@benramsey.com>
2018-02-04 21:34:51 -0600

    Configure application logging with Papertrail

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\SyslogUdpHandler;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Papertrail logging
+        $papertrailUrl = env('PAPERTRAIL_URL', '');
+        $papertrailPort = env('PAPERTRAIL_PORT');
+
+        if (filter_var($papertrailUrl, FILTER_VALIDATE_URL)) {
+            $output = "%channel%.%level_name%: %message%";
+            $formatter = new LineFormatter($output);
+            $logger = Log::getMonolog();
+            $syslogHandler = new SyslogUdpHandler($papertrailUrl, $papertrailPort);
+            $syslogHandler->setFormatter($formatter);
+            $logger->pushHandler($syslogHandler);
+        }
     }
 }
