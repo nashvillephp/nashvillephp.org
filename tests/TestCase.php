079c9265611b913d63d2234c090a0fe7c0234e5b
Ben Ramsey <ben@benramsey.com>
2018-02-24 18:00:01 -0600

    Clean up after Mockery in the base tearDown()

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,17 @@
 <?php
+declare(strict_types=1);
 
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Mockery;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function tearDown()
+    {
+        Mockery::close();
+    }
 }
