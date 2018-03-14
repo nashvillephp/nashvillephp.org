9fc0152ff5f5de3e7d143b9fe2069376e77c2e12
Ben Ramsey <ben@benramsey.com>
2018-03-13 19:17:28 -0500

    Implement response caching

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'cacheResponse' => \Spatie\ResponseCache\Middlewares\CacheResponse::class,
+        'doNotCacheResponse' => \Spatie\ResponseCache\Middlewares\DoNotCacheResponse::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,31 @@
 |
 */
 
-Route::get('/', 'PageController@home')->name('home');
-Route::get('/about', 'PageController@about')->name('about');
-Route::get('/sponsors', 'PageController@sponsors')->name('sponsors');
-Route::get('/speakers', 'PageController@speakers')->name('speakers');
-Route::get('/speak', 'ProposalController@proposalForm')->name('speak');
-Route::post('/speak', 'ProposalController@processProposal');
+$cacheTtl = [
+    '12hours' => 60 * 12,
+    'day' => 60 * 24,
+    'week' => 60 * 24 * 7,
+];
 
-Route::get('/articles', function () {
-    return view('article-list');
+Route::get('/', 'PageController@home')
+    ->name('home')
+    ->middleware("cacheResponse:{$cacheTtl['12hours']}");
+
+Route::middleware(["cacheResponse:{$cacheTtl['week']}"])->group(function () {
+    Route::get('/about', 'PageController@about')->name('about');
+    Route::get('/sponsors', 'PageController@sponsors')->name('sponsors');
+    Route::get('/speakers', 'PageController@speakers')->name('speakers');
+
+    Route::get('/articles', function () {
+        return view('article-list');
+    });
+
+    Route::get('/articles/1234', function () {
+        return view('article');
+    });
 });
 
-Route::get('/articles/1234', function () {
-    return view('article');
+Route::middleware(['doNotCacheResponse'])->group(function () {
+    Route::get('/speak', 'ProposalController@proposalForm')->name('speak');
+    Route::post('/speak', 'ProposalController@processProposal');
 });
